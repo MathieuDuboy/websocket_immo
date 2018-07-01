8c4b22b52237eadaf8818933da397f26f634a51e
MathieuDuboy <11943636+MathieuDuboy@users.noreply.github.com>
2018-07-01 11:51:44 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,74 @@
+const LILapi = require('./calc/LILapi.js');
+var TT = new LILapi();
+module.exports = {
+   getDay: function(date, ville) {
+      var tt = []
+      for (var i = 0; i < 86400; i++) tt.push(TT.Ask(ville, date, i));
+      return tt;
+   },
+   getMsSinceMidnight: function() {
+      var now = new Date();
+      var hours = now.getHours() * (60 * 60);
+      var minutes = now.getMinutes() * 60;
+      var seconds = now.getSeconds();
+      var secSinceMidnight = hours + minutes + seconds;
+      return secSinceMidnight;
+   },
+   generate_datas_instant: function(date, ville) {
+      var date = date;
+      var ville = ville;
+      var i = module.exports.getMsSinceMidnight();
+      console.log("secondes depuis minuit : " + i);
+      var indice_actuel = TT.Ask(ville, date, i);
+      var datas_return = {
+         "indice_actuel": indice_actuel,
+         "ville": ville,
+         "date": date
+      };
+      return datas_return;
+   },
+   generate_datas_today: function(date, ville) {
+      var date = date;
+      var ville = ville;
+      var totalsecondes = module.exports.getMsSinceMidnight();
+      var datas_return = {
+         "ville": ville,
+         "date": date,
+         "datas": []
+      };
+      for (var i = 0; i < totalsecondes; i++) {
+         var valeur = TT.Ask(ville, date, i);
+         datas_return.datas.push({
+            "indice": valeur
+         });
+      }
+      return datas_return;
+   },
+   generate_datas_day: function(date, ville) {
+      var date = date;
+      var ville = ville;
+      var indices_open_close = TT.GetOpenPricesDate(ville, date);
+      var datas_return = {
+         "ville": ville,
+         "date": date,
+         "indice_ouverture": indices_open_close[1],
+         "indice_fermeture": indices_open_close[2],
+         "datas": []
+      };
+      for (var i = 0; i < 86400; i++) {
+         var valeur = TT.Ask(ville, date, i);
+         datas_return.datas.push({
+            "indice": valeur
+         });
+      }
+      return datas_return;
+   },
+   generate_datas_year: function(ville) {
+      var indices_open_close = TT.GetOpenPricesTop(ville);
+      var datas_return = {
+         "ville": ville,
+         "datas": indices_open_close
+      };
+      return datas_return;
+   }
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var axios = require('axios');
+var jwt = require('jsonwebtoken');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var functions = require('./functions.js');
+// calculateur Lilian init
+const LILapi = require('./calc/LILapi.js');
+var TT = new LILapi();
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+io.use(function(socket, next) {
+   if (socket.handshake.query && socket.handshake.query.token) {
+      jwt.verify(socket.handshake.query.token, 'shhhhh', function(err, decoded) {
+         if (err) return next(new Error('Authentication error'));
+         socket.decoded = decoded;
+         next();
+      });
+   } else {
+      next(new Error('Authentication error'));
+   }
+}).on('connection', function(socket) {
+   console.log("Connected succesfully to the socket ..." + JSON.stringify(socket.handshake.query));
+   if (socket.handshake.query.route == 'now') {
+      setInterval(() => {
+         socket.emit('now', functions.generate_datas_instant(socket.handshake.query.date, socket.handshake.query.ville));
+      }, 1000);
+   } else if (socket.handshake.query.route == 'today') {
+      setInterval(() => {
+         socket.emit('today', functions.generate_datas_today(socket.handshake.query.date, socket.handshake.query.ville));
+      }, 1000);
+   } else if (socket.handshake.query.route == 'day') {
+      socket.emit('day', functions.generate_datas_day(socket.handshake.query.date, socket.handshake.query.ville));
+   } else if (socket.handshake.query.route == 'year') {
+      socket.emit('year', functions.generate_datas_year(socket.handshake.query.ville));
+   }
+   socket.on('disconnect', function() {
+      console.log('Got disconnect!');
+   });
+});
